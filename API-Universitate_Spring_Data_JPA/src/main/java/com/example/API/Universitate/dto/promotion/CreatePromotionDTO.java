0315ef3e2ef31cb0c5860e1e597d74fc14a9526f
Ghertner Arthur <arthur94sv@gmail.com>
2020-03-15 19:20:48 +0200

    modified validation for DTO

--- API-Universitate_Spring_Data_JPA/src/main/java/com/example/API/Universitate/dto/promotion/CreatePromotionDTO.java
+++ API-Universitate_Spring_Data_JPA/src/main/java/com/example/API/Universitate/dto/promotion/CreatePromotionDTO.java
@@ -1,18 +1,26 @@
 package com.example.API.Universitate.dto.promotion;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 
 public class CreatePromotionDTO {
-    @Min(value = 4, message = "Anul de inceput trebuie sa aiba 4 cifre")
+
+    @NotNull(message = "Anul de inceput trebuie sa aiba 4 cifre")
+    @Min(4)
     public int startYear;
 
-    @Min(value = 4, message = "Anul de sfarsit trebuie sa aiba 4 cifre")
+
+    @NotNull(message = "Anul de sfarsit trebuie sa aiba 4 cifre")
+    @Min(4)
     public int endYear;
 
+    @NotNull
     @Min(value = 1, message = "Trebuie sa existe minim 1 loc disponibil la buget")
     public int nrLocuriBuget;
 
+    @NotNull
     @Min(value = 1, message = "Trebuie sa existe minim 1 loc disponibil la taxa")
     public int nrLocuriTaxa;
 }
